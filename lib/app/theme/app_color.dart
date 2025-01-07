1f4899c7267600dbde4fd536143ac091207230cf
Hamul777 <rhrb1162@gmail.com>
2025-01-07 16:16:09 +0900

    [Add] 어플 color 설정

---
+++ lib/app/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  AppColor._();
+
+  static const navy = Color(0xFF112678);
+  static const blue = Color(0xFF1A5890);
+  static const skyBlue = Color(0xFF2889C1);
+  static const back = Color(0xFFF4F4F4);
+  static const input = Color(0xFFF8F8F8);
+  static const lightGray = Color(0xFF808080);
+  static const gray = Color(0xFF2E2E2D);
+}
