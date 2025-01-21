51d5a4b49f2da74c9e32a906d05176042d3e94d0
Hamul777 <rhrb1162@gmail.com>
2025-01-21 21:34:54 +0900

    [style] 라우터 수정

--- lib/comm/setting/navigator_named.dart
+++ lib/comm/setting/navigator_named.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ncs/features/exhibits/presentation/pages/3F/big_history_main.dart';
-import 'package:ncs/features/exhibits/presentation/pages/4F/cosmos_main.dart';
-import 'package:ncs/features/exhibits/presentation/pages/5F/scope_main.dart';
 import 'package:ncs/features/navigation/presentation/widgets/common_bottom_nav.dart';
+import 'package:ncs/features/notifications/presentation/pages/alarm_main.dart';
 
 class NavigatorNamed {
   Map<String, Widget Function(BuildContext)> named = {
 
     '/home': (context) => const CommonBottomNav(),
 
-    '/cosmos': (context) => const CosmosMain(),
-
-    '/bigHistory': (context) => const BigMain(),
-
-    '/scope': (context) => const ScopeMain(),
-
+    '/alarm': (context) => const AlarmMain(),
   };
 }
