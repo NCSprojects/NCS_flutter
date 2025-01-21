0db96463f5b30fcf2c8034bfcd4cb4398fecfea3
Hamul777 <rhrb1162@gmail.com>
2025-01-21 21:31:46 +0900

    [delete] runApp

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,14 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ncs/app/theme/app_color.dart';
 import 'package:ncs/features/home/widgets/main_container_off.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const HomePage());
-}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,28 +9,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: AppColor.back,
         body: Center(child: MainContainerOff(title: '입장권',))
       ),
     );
   }
 }
-//
-//
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-//
-//   // 화면
-//   @override
-//   Widget build(BuildContext context) {
-//     throw UnimplementedError();
-//   }
-//
-// }
